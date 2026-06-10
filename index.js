513ebbee1f7e25fe3d96456ec463d64347b0fc6e
Darshini <117499914+darshini-66@users.noreply.github.com>
2026-06-11 00:56:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,16 +68,6 @@ card.style.transitionDelay =
 
 /* CONTACT FORM */
 
-const form =
-document.querySelector('.contact-form');
-
-form.addEventListener('submit',(e)=>{
-
-e.preventDefault();
-
-alert(
-'Thank you for your message! I will get back to you soon.'
-);
 
 form.reset();
 
